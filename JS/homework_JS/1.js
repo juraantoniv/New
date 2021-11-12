bd78b6a1c159b36e77c3aad255f14e25ad23a1c2
juraantoniv <juraantoniv@gmail.com>
2021-11-12 17:51:56 +0200

    JS

---
+++ JS/homework_JS/1.js
@@ -0,0 +1,55 @@
+//let a = 'hello'
+//b = 'owu'
+//c = 'com'
+//d = 'ua'
+//e = 1
+//f = 10
+//g = -999
+//e = 123
+//k = 3.14
+//j = 2.7
+//l = 16
+//w = true
+//v = false
+
+//console.log(a)
+//alert(a)
+//document.write(a)
+
+//b = 'owu1'
+//c = 'com1'
+//d = 'ua1'
+//e = 1
+//f = 10
+//g = -99
+//e = 12
+//k = 3.5
+//j = 2.8
+//l = 16
+//w = false
+//v = true
+
+//let firstName
+//let middleName
+//let lastName
+//firstName = 'Yyra'
+//lastName = 'Antoniv'
+//middleName ='Vasylovysh'
+//let FullName = firstName + middleName + lastName
+//console.log(FullName)
+
+//let k = 100;
+//let b = '100';
+//let c = true;
+
+//console.log(typeof k)
+//console.log(typeof b)
+//console.log(typeof c)
+
+//let name = prompt('Введіть імя');
+//let surname = prompt('Введіть  призвіще');
+//let Age = prompt('Введіть вік ваш');
+
+//('Ваше імя'+' '+ name)
+//alert('Ваше призвіще'+' '+ surname)
+//alert('Ваше вік'+' '+ Age )
